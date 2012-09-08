7b345181f1bc7e8bf2816811132905fb6abd4015
Zeh Rizzatti <zehrizzatti@gmail.com>
2012-09-08 00:34:45 -0400

    change install system

--- install.sh
+++ install.sh
@@ -4,8 +4,14 @@ set -e
 
 # creates symlinks inside the home directory
 dir=`echo $(pwd) | sed "s|$HOME/||"`
-cd $HOME
-ln -fs $dir .vim
-for file in vimrc gvimrc; do
-  ln -fs .vim/$file .$file
-done
+(
+  cd $HOME
+  ln -fs $dir .vim
+  for file in $dir/dotfiles/*; do
+    realfile=`greadlink -f $file | sed "s|$HOME/||"`
+    linkname=".`basename $file`"
+    ln -fs $realfile $linkname
+  done
+)
+
+sh toggle_colorscheme.sh
